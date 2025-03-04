137eb7b751fb84eaff0afe0699c639d24cd80102
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2025-03-04 16:02:10 -0700

    added terminal installation to the setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install openbox firefox thunar tigervnc-standalone-server 
+sudo apt install openbox firefox thunar tigervnc-standalone-server konsole
 git clone https://github.com/novnc/noVNC
 clear
 echo("✅ Setup Complete! Start by running start.sh")
