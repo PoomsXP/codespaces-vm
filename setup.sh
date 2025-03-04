35ce9260b9ac27f974654d6045d03fc6f91d7ba7
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2025-03-04 15:40:06 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install openbox firefox thunar tigervnc-standalone-server 
+git clone https://github.com/novnc/noVNC
+clear
+echo("✅ Setup Complete! Start by running start.sh")
