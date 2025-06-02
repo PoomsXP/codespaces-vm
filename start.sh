f418d13d718ddcbea7df6d433682098496ac9248
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2025-06-01 20:38:24 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@ cd noVNC
 sudo vncserver -SecurityType none -xstartup "openbox" -rfbport 5900
 sudo ./utils/novnc_proxy --vnc 127.0.0.1:5900 --listen localhost:6080
 clear
+echo
+echo
 echo ✅ Start Complete! Navigate to 127.0.0.1:5900/vnc.html to connect.
+echo
+
