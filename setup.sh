8446c3bf465b6159e3348de702cc2b05d70fe659
PoomsXP <115908009+PoomsXP@users.noreply.github.com>
2025-09-07 11:08:47 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 sudo apt update
-sudo apt install openbox firefox thunar tigervnc-standalone-server konsole
+sudo apt install openbox thunar tigervnc-standalone-server konsole alien
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
 git clone https://github.com/novnc/noVNC
 clear
 echo
