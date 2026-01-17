24c55796c45bf31a2eea103900c57fee2ea405f6
PoomsXP <115908009+PoomsXP@users.noreply.github.com>
2026-01-16 20:20:42 -0800

    Add '-y' flag to apt install commands

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 sudo apt update
-sudo apt install openbox thunar tigervnc-standalone-server konsole alien xfce4 xfce4-session xfce4-terminal xfce4-panel xfdesktop4 thunar x11vnc xvfb dbus-x11
+sudo apt install openbox thunar tigervnc-standalone-server konsole alien xfce4 xfce4-session xfce4-terminal xfce4-panel xfdesktop4 thunar x11vnc xvfb dbus-x11 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome*.deb
+sudo apt install ./google-chrome*.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
 sudo chmod +x start.sh
-sudo chmod +x openbox-start.sh
 git clone https://github.com/novnc/noVNC
 clear
 echo
