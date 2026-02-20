5a3b6600baa0214cdf9c6731e517480fa362831f
PoomsXP <115908009+PoomsXP@users.noreply.github.com>
2026-02-20 15:44:35 -0700

    Remove Chrome and Add Brave and Firefox

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 sudo apt update
-sudo apt install openbox thunar tigervnc-standalone-server konsole alien xfce4 xfce4-session xfce4-terminal xfce4-panel xfdesktop4 thunar x11vnc xvfb dbus-x11 -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome*.deb -y
-sudo rm google-chrome-stable_current_amd64.deb
+sudo apt install openbox thunar tigervnc-standalone-server konsole alien xfce4 xfce4-session xfce4-terminal xfce4-panel xfdesktop4 thunar x11vnc xvfb dbus-x11 firefox -y
+curl -fsS https://dl.brave.com/install.sh | sh
 sudo chmod +x start.sh
 git clone https://github.com/novnc/noVNC
 clear
